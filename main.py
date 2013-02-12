c714cdafe05114cbffc605cdef57525e5c1385ac
gnrr <hideaki.sh@gmail.com>
2013-02-12 13:00:46 +0900

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+greeting = 'hi!'
+vocabulary = ['hello']
+
+
+# main program
+
+print greeting
+
+if __name__ == '__main__':
+	while 1:
+		print raw_input(">")
+		print vocabulary[0]
+		print
