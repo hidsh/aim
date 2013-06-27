52629d9603760a5c52382a7995628ef2fe781ede
gnrr <hideaki.sh@gmail.com>
2013-06-27 18:54:33 +0900

    fixed: encoding

--- denken/markdown_test.py
+++ denken/markdown_test.py
@@ -6,8 +6,9 @@ import markdown
 ##
 if __name__ == '__main__':
     html_str = '<html><header><meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></header><body>'
-    md_str = open('test.md').read()
+
+    md_str = open('test.md', 'r', encoding='utf-8').read()
     html_str += markdown.markdown(md_str)
     html_str += '</body></html>'
 
-    open('test2.html', 'w').write(html_str)
+    open('test2.html', 'w', encoding='utf-8').write(html_str)
