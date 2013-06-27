452a1a1146935fd9fe3d35c1fa0c0aeda3968b36
gnrr <hideaki.sh@gmail.com>
2013-06-27 18:46:13 +0900

    add: markdown test

---
+++ denken/markdown_test.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import markdown
+
+##
+if __name__ == '__main__':
+    html_str = '<html><header><meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></header><body>'
+    md_str = open('test.md').read()
+    html_str += markdown.markdown(md_str)
+    html_str += '</body></html>'
+
+    open('test2.html', 'w').write(html_str)
