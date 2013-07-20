6625f8349b14b789e773265e7944a3b73da4eb2f
gnrr <hideaki.sh@gmail.com>
2013-07-20 17:10:49 +0900

    add: lib directory

---
+++ drill/driller/lib/html_entity.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# http://www.benricho.org/symbol/tokusyu_07_mark.html
+
+class HTML_Entity(object):
+    CHECK = '&#10003;'
+    PEKE = '&#10005;'
+    MARU = '&#9711;'
+
+##
+if __name__ == '__main__':
+    pass
