1e65a40627c1483b61e3a40e303ebf22ea8d2b3b
gnrr <hideaki.sh@gmail.com>
2013-08-27 15:17:15 +0900

    fixed: rpi cannot save user data
    Mac から rpi に移動すると user:__gのファイルパスをMacのままなのでrpi で FileNotFound
    となってsaveできない。
    rpi側で installするときにユーザデータをリストアするようにした。

--- drill_install.sh
+++ drill_install.sh
@@ -1,2 +1,12 @@
+if [ -e "drill/user/__g" ]; then
+  mv drill/user/__g .
+fi
+
 rm -fr drill
 tar zxvf drill.tar.gz
+
+if [ -e "./__g" ]; then
+    mv ./__g drill/user/
+else
+    rm drill/user/__g
+fi
