40a0d3e123d1f6ec3d63910d84a0db714f4bbf0f
gnrr <hideaki.sh@gmail.com>
2013-09-30 17:14:59 +0900

    end of チュートリアルその2

--- web-framework-test/django_test_0/polls/models.py
+++ web-framework-test/django_test_0/polls/models.py
@@ -12,6 +12,7 @@ class Poll(models.Model):
 
     def was_published_today(self):
         return self.pub_date.date() == datetime.date.today()
+    was_published_today.short_description = 'Published today?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
@@ -20,3 +21,28 @@ class Choice(models.Model):
         
     def __repr__(self):
         return  self.choice
+
+
+from django.contrib import admin
+# admin.site.register(Poll)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question']
+    fieldsets = [
+        (None, {'fields':['question']}),
+        ('DateInformation', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_today')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
+
+# from polls.models import Choice
+# admin.site.register(Choice)
