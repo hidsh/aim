2a458d10782a509bae20161839abbd4c5bca626c
gnrr <hideaki.sh@gmail.com>
2013-10-01 11:37:16 +0900

    end of チュートリアルその4

--- web-framework-test/django_test_0/polls/urls.py
+++ web-framework-test/django_test_0/polls/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
+
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_test_0.views.home', name='home'),
     # url(r'^django_test_0/', include('django_test_0.foo.urls')),
@@ -15,8 +18,20 @@ urlpatterns = patterns('polls.views',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    (r'^$', 'index'),
-    (r'^(?P<poll_id>\d+)/$', 'detail'),
-    (r'^(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+
 )
--- web-framework-test/django_test_0/polls/views.py
+++ web-framework-test/django_test_0/polls/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 # from django.template import Context, loader
-from polls.models import Poll
+from polls.models import Poll, Choice
 # from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
-
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+"""
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     # t = loader.get_template('polls/index.html')
@@ -12,7 +14,7 @@ def index(request):
     #         'latest_poll_list':latest_poll_list,
     # })
     # return HttpResponse(t.render(c))
-    return render_to_response('polls/index.html', {'latest_poll_list':latest_poll_list})
+    return render_to_response('polls/poll_list.html', {'latest_poll_list':latest_poll_list})
 
 # from django.http import Http404
 def detail(request, poll_id):
@@ -24,3 +26,25 @@ def detail(request, poll_id):
 
     p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll':p})
+"""
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/poll_detail.html', {
+                  'object':p,
+                  'error_message':'選択肢を選んでいません。',
+                })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+
+"""
+def results(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll':p})
+"""
+
