f9eb061eab137f8b7889b3047e89d7d09a9851db
gnrr <hideaki.sh@gmail.com>
2013-10-02 19:30:03 +0900

    end of 前編 6/6
    http://codezine.jp/article/detail/4065?p=6

--- web-framework-test/ecsite/itempage/views.py
+++ web-framework-test/ecsite/itempage/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.template import Context, loader
+from models import Item
+
+def item_page_display(request,item_id):
+    # item_idに該当するオブジェクトを取得する
+    item = Item.objects.get(id=item_id)
+    # テンプレートを取得して、モデルの値とマージする
+    t = loader.get_template('page/item.html')
+    c = Context(
+        {'item':item }
+    )
+    # HTTP Responseを返す。
+    return HttpResponse(t.render(c))
+
--- web-framework-test/ecsite/settings.py
+++ web-framework-test/ecsite/settings.py
@@ -71,6 +71,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/g/test/python/web/aim/web-framework-test/ecsite/templates',
 )
 
 INSTALLED_APPS = (
