5e93753dae1c8f3a90476a3179157c930dd92ded
gnrr <hideaki.sh@gmail.com>
2013-10-05 11:28:04 +0900

    end of 後編 2/6
    http://codezine.jp/article/detail/4264?p=2

--- web-framework-test/ecsite/itempage/admin.py
+++ web-framework-test/ecsite/itempage/admin.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from ecsite.itempage.models import Item
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('item_code', 'item_name', 'price')
+    
+admin.site.register(Item, ItemAdmin)
 # import pdb; pdb.set_trace()
 
 
--- web-framework-test/ecsite/itempage/views.py
+++ web-framework-test/ecsite/itempage/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context, loader
 from models import Item
 
 def item_page_display(request,item_id):
-    # item_idに該当するオブジェクトを取得する
-    item = Item.objects.get(id=item_id)
+    try:
+        # item_idに該当するオブジェクトを取得する
+        item = Item.objects.get(id=item_id)
+    except Item.DoesNotExist:
+        raise Http404
+    
     # テンプレートを取得して、モデルの値とマージする
     t = loader.get_template('page/item.html')
     c = Context(
