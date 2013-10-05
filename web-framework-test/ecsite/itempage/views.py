7ee4f836f9a9cb6a536ce38810021b2221b722e3
gnrr <hideaki.sh@gmail.com>
2013-10-05 11:32:16 +0900

    end of 後編 3/6
    http://codezine.jp/article/detail/4264?p=3

--- web-framework-test/ecsite/itempage/views.py
+++ web-framework-test/ecsite/itempage/views.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.template import Context, loader
 from models import Item
 
 def item_page_display(request,item_id):
-    try:
-        # item_idに該当するオブジェクトを取得する
-        item = Item.objects.get(id=item_id)
-    except Item.DoesNotExist:
-        raise Http404
-    
-    # テンプレートを取得して、モデルの値とマージする
-    t = loader.get_template('page/item.html')
-    c = Context(
-        {'item':item }
-    )
-    # HTTP Responseを返す。
-    return HttpResponse(t.render(c))
+    item = get_object_or_404(Item, id=item_id)
 
+    return direct_to_template(request, 'page/item.html',
+                              extera_content={'item':item}
+    )
